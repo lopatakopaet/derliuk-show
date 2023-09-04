066f94942c4a15d9d0e39024f770fb1233593f6e
Dmitriy Akymov <Dmitriy77788@gmail.com>
2023-09-04 15:30:20 +0300

    added comment style, fixed header style, add i18.json

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {I18nService} from "../../services/i18n.service";
+import * as lang from "../../i18n/i18n.json";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  // lang: string;
+
+  constructor(public i18n: I18nService) {
+    // this.lang = i18n.lang
+  }
+
+
 
   ngOnInit(): void {
+  }
+
+  setLang(langName: string): void {
+    this.i18n.changeLang(langName);
+    // this.lang = this.i18n.lang;
   }
 
 }
